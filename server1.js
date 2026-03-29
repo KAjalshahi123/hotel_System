f35f1f0b96059d79924fa4cce1a6b95dd5bb93bd
KAjalshahi123 <shahikajal274508@gmail.com>
2026-03-29 21:52:07 +0530

    Initial commit - Node.js server

---
+++ server1.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const app =  express();
+app.get('/', function(req, res){
+    res.send("Hello World how can i help  you ");
+
+})
+
+app.get('/chiken', (req, res) => {
+    res.send("you can eat chiken");
+}
+)
+
+app.get('/idl', (req, res)=> {
+    var order = {
+        name : "idldal",
+        price: 100,
+        quantity: 1
+    }
+    res.send(order);
+})
+app.listen(3000)
